b28857fce59df5f68dff73392460c596243e5dfa
Harindu Liyanage <harindu@surge.global>
2024-03-20 10:57:17 +0530

    initial commit

--- src/controller/login.controller.js
+++ src/controller/login.controller.js
@@ -11,7 +11,7 @@ export const login = catchAsync(async (req, res) => {
     };
 
     const response = await axios
-        .get(`http:/keycloak:8080/realms/phg/broker/PreferredNet/login?client_id=${client_id}&tab_id=${tab_id}&session_code=${session_code}`,
+        .get(`http://keycloak:8080/realms/phg/broker/PreferredNet/login?client_id=${client_id}&tab_id=${tab_id}&session_code=${session_code}`,
             config);
 
     console.log(response);
