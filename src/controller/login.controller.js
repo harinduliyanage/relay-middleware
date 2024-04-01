89fed058868bdf3bdc74478d2eadb2ad6eb0ac58
Nipuna Akalana <akalana.nipuna@gmail.com>
2024-04-01 22:57:11 +0530

    Code refactor

--- src/controller/login.controller.js
+++ src/controller/login.controller.js
@@ -6,13 +6,16 @@ import { appendingRequestId, decodeAuthRequestId } from "../utils/saml-parser";
 let state = "";
 let requestId = "";
 
+// Login route handler
 export const login = catchAsync(async (req, res) => {
+  // Extract required parameters from the request query
   const { client_id, tab_id, session_code } = pick(req.query, [
     "client_id",
     "tab_id",
     "session_code",
   ]);
-  //
+
+  // Configure axios request
   const config = {
     headers: req.headers,
     maxRedirects: 0,
@@ -21,11 +24,13 @@ export const login = catchAsync(async (req, res) => {
     },
   };
 
+  // Perform GET request to Keycloak login endpoint
   const response = await axios.get(
     `http://keycloak:8080/realms/phg/broker/PreferredNet/login?client_id=${client_id}&tab_id=${tab_id}&session_code=${session_code}`,
     config
   );
 
+  // Extract required data from response headers
   const locationHeader = response.headers["location"];
   if (locationHeader) {
     const urlParams = new URLSearchParams(
@@ -35,26 +40,25 @@ export const login = catchAsync(async (req, res) => {
     const authRequest = urlParams.get("SAMLRequest");
     requestId = await decodeAuthRequestId(authRequest);
   }
-  console.log("current state:", state);
-  console.log("current auth request id:", requestId);
+
+  // Redirect to the obtained location
   res.redirect(301, locationHeader);
 });
 
+// Single Sign-On callback route handler
 export const ssoCallBack = catchAsync(async (req, res) => {
-  //
-  //   console.log("req - h", req.headers);
-  //   console.log("req", req.body);
+  // Extract SAMLResponse from the request body
   const { SAMLResponse } = req.body;
 
+  // Append request ID to SAMLResponse
   const amendedRes = await appendingRequestId(atob(SAMLResponse), requestId);
-  //   console.log("amendedRes", amendedRes);
-  //   console.log("amendedRes encoded", btoa(amendedRes));
 
-  // Prepare form data payload for Axios request to Keycloak
+  // Prepare form data payload for Axios request to Keycloak endpoint
   const formData = new URLSearchParams();
   formData.append("SAMLResponse", btoa(amendedRes));
   formData.append("RelayState", state);
-  //
+
+  // Configure axios request
   const config = {
     headers: req.headers,
     maxRedirects: 0,
@@ -62,28 +66,23 @@ export const ssoCallBack = catchAsync(async (req, res) => {
       return status === 200 || status === 302 || status === 301;
     },
   };
-  //   console.log("formData ", formData);
+
   try {
+    // Perform POST request to Keycloak endpoint
     const response = await axios.post(
       "http://keycloak:8080/realms/phg/broker/PreferredNet/endpoint",
       formData,
       config
     );
-    console.log("==============");
-    console.log(response.headers);
-    console.log(response.headers.location);
-    // console.log("response headers", response.headers);
-    // console.log("response data", response.data);
-    // Forward Keycloak response to client
+
+    // Set response headers and redirect
     res.headers = response.headers;
     res.redirect(302, response.headers.location);
-    // res.status(response.status).send(response?.data);
-  } catch (e) {
-    console.log("error", e);
-    if (e?.response?.headers) {
-      res.headers = e.response.headers;
+  } catch (error) {
+    // If an error occurs, handle it and redirect accordingly
+    if (error?.response?.headers) {
+      res.headers = error.response.headers;
     }
-    res.redirect(301, response.response.headers["location"]);
-    // res.redirect(e?.response.status).send(e.response.data);
+    res.redirect(302, res.headers.location);
   }
 });
